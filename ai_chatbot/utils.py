eeabb3d74f429216da3c915f30deeb84394cde88
Shru1308 <96241733+Shru1308@users.noreply.github.com>
2024-07-27 12:53:26 +0530

    Update utils.py

--- ai_chatbot/utils.py
+++ ai_chatbot/utils.py
@@ -1,9 +1,9 @@
-# Standard library import
-import logging
+# set up for sending WhatsApp message through Twilio API
 
-# Third-party imports
+
+import logging
 from twilio.rest import Client
-from decouple import config      #used to store private credentials in a .env file
+from decouple import config      #used to access private credentials stored in .env file
 
 
 account_sid = config("TWILIO_ACCOUNT_SID")
@@ -11,11 +11,11 @@ auth_token = config("TWILIO_AUTH_TOKEN")
 client = Client(account_sid, auth_token)
 twilio_number = config('TWILIO_NUMBER')
 
-# Setting up logging
+# Configuring the logging
 logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)     # creating a logger object
 
-# Sending message logic through Twilio Messaging API
+# Sending message through Twilio API
 def send_message(to_number, body_text):
     try:
         message = client.messages.create(
